0cd0ab7ac242e34448033cf74ce85f7da7b4a8ce
Moazam05 <salmanmoazam08@gmail.com>
2023-11-24 00:28:06 +0500

    routes define

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,24 @@
-import { Box } from "@mui/material";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./views/Home";
+import NotFound from "./views/NotFound";
+import Login from "./views/Login";
+import SignUp from "./views/SignUp";
+import Profile from "./views/Profile";
+import About from "./views/About";
 
-export default function App() {
+function App() {
   return (
-    <div>
-      <Box>App</Box>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
   );
 }
+
+export default App;
